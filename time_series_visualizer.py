afd9eb827bea641485df91894e33ff828172c94e
David Fernandez Brito <daviferbri@gmail.com>
2025-02-24 18:59:08 +0000

    Fixed a bug caused by np.float deprecation in NumPy 1.20+, added a patch for compatibility.

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -4,6 +4,9 @@ import seaborn as sns
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
+import numpy as np
+np.float = float
+
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv("fcc-forum-pageviews.csv", parse_dates= ["date"], index_col = "date")
 
